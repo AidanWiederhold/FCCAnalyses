7feeda85faf3d051da8b2f4517df3f0848382492
github-action-benchmark <github@users.noreply.github.com>
2022-06-10 13:30:41 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 4a1fbf75148757eeb47c545f618fff6e205665f1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1654867824131,
+  "lastUpdate": 1654867840948,
   "repoUrl": "https://github.com/AidanWiederhold/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -216,6 +216,44 @@ window.BENCHMARK_DATA = {
             "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
             "value": 5.838123546507946,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aidanwiederhold@gmail.com",
+            "name": "Aidan Wiederhold",
+            "username": "AidanWiederhold"
+          },
+          "committer": {
+            "email": "aidanwiederhold@gmail.com",
+            "name": "Aidan Wiederhold",
+            "username": "AidanWiederhold"
+          },
+          "distinct": true,
+          "id": "4a1fbf75148757eeb47c545f618fff6e205665f1",
+          "message": "Move everything for the analysis to a single directory.",
+          "timestamp": "2022-06-10T14:21:38+01:00",
+          "tree_id": "c25aad011e184eafa99f5c594e722763bfb459df",
+          "url": "https://github.com/AidanWiederhold/FCCAnalyses/commit/4a1fbf75148757eeb47c545f618fff6e205665f1"
+        },
+        "date": 1654867839339,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 13.086860179901123,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 15.856339931488037,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
           }
         ]
       }
