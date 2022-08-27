705b29a5c6e742e99229d99d753b63e6a18e3178
github-action-benchmark <github@users.noreply.github.com>
2022-08-27 22:26:26 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for e437cb235a829d142041476b80be781d00b5d5ff

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1661639185158,
+  "lastUpdate": 1661639186435,
   "repoUrl": "https://github.com/AidanWiederhold/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -837,6 +837,45 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aidanwiederhold@gmail.com",
+            "name": "Aidan Wiederhold",
+            "username": "AidanWiederhold"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e437cb235a829d142041476b80be781d00b5d5ff",
+          "message": "Merge pull request #1 from AidanWiederhold/aiwieder/b2snunu-tidy_up\n\nAiwieder/b2snunu tidy up",
+          "timestamp": "2022-08-28T00:15:13+02:00",
+          "tree_id": "71c3cfeb864ceb2587875dcdad7f5de57075a9b0",
+          "url": "https://github.com/AidanWiederhold/FCCAnalyses/commit/e437cb235a829d142041476b80be781d00b5d5ff"
+        },
+        "date": 1661639186026,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 7.2452730481402785,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 5.448670566180545,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test",
+            "value": 7.328330744375495,
+            "unit": "Evt/s"
           }
         ]
       }
